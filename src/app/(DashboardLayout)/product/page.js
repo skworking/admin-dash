350f9d42d1b1b96ba9478aa62311e76abcb80981
satish-kumar <satishgupta9792070@gmail.com>
2024-04-09 12:29:53 +0530

    first commit

---
+++ src/app/(DashboardLayout)/product/page.js
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { Container, Col, Row, Card, CardBody, CardTitle } from "reactstrap";
+const Product = () => {
+  return (
+    <>
+      <Card >
+       <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+        Product
+        </CardTitle>
+      </Card>
+    </>
+  )
+}
+export default Product;
