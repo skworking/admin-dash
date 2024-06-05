0da97ae3cde8f06d5c776e1fe56dd6d749d79a11
server <satishgupta9792070@gmail.com>
2024-06-05 18:19:15 +0530

    create page for compare-details

--- src/app/[locale]/(DashboardLayout)/(User)/compare/[compare-name]/page.js
+++ src/app/[locale]/(DashboardLayout)/(User)/compare/[compare-name]/page.js
@@ -1,7 +1,11 @@
+'use client'
+import useProductStore from "@/store/productStrore";
 const CompareDetails=()=>{
+    const {truck1,truck2}=useProductStore();
     return(
         <>
-        hii
+        <h1>Comparation between Trucks</h1>
+            {truck1?.name} - {truck2?.name}
         </>
     )
 }
